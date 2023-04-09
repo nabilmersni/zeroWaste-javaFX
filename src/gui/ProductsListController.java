fca99616b9ad6c58ccdeb60c38067d5850d7d542
Ali <braiekali123@gmail.com>
2023-04-09 01:22:19 +0100

    générer pdf (liste produits)

--- src/gui/ProductsListController.java
+++ src/gui/ProductsListController.java
@@ -2,6 +2,7 @@ package gui;
 
 import java.io.IOException;
 import java.net.URL;
+import java.time.LocalDate;
 import java.util.HashMap;
 import java.util.List;
 import java.util.Map;
@@ -9,6 +10,7 @@ import java.util.ResourceBundle;
 
 import entities.Categorie_produit;
 import entities.Produit;
+import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
 import javafx.fxml.FXMLLoader;
 import javafx.fxml.Initializable;
@@ -27,6 +29,23 @@ import services.Categorie_produitService;
 import services.ICategorie_produitService;
 import services.IProduitService;
 import services.ProduitService;
+
+import com.itextpdf.text.BaseColor;
+import com.itextpdf.text.Document;
+import com.itextpdf.text.Element;
+import com.itextpdf.text.Font;
+import com.itextpdf.text.Image;
+import com.itextpdf.text.Paragraph;
+import com.itextpdf.text.Phrase;
+import com.itextpdf.text.pdf.PdfPCell;
+import com.itextpdf.text.pdf.PdfPTable;
+import com.itextpdf.text.pdf.PdfWriter;
+import javafx.stage.FileChooser;
+import javafx.stage.FileChooser.ExtensionFilter;
+import java.io.File;
+import java.io.FileOutputStream;
+import javafx.scene.Node;
+
 
 
 /**
@@ -274,6 +293,210 @@ public class ProductsListController implements Initializable {
         productsListContainer.getChildren().clear();
         this.setProductGridPaneList();
 
+    }
+
+    @FXML
+    void genererPdf(ActionEvent event) {
+             // Afficher la boîte de dialogue de sélection de fichier
+          /*    FileChooser fileChooser = new FileChooser();
+             fileChooser.setTitle("Enregistrer le fichier PDF");
+             fileChooser.getExtensionFilters().add(new ExtensionFilter("Fichiers PDF", "*.pdf"));
+             File selectedFile = fileChooser.showSaveDialog(((Node) event.getSource()).getScene().getWindow());
+             
+             if (selectedFile != null) {
+                 // Générer le fichier PDF avec l'emplacement de sauvegarde sélectionné
+                 generatePDF(selectedFile);
+             }
+     */
+    }
+
+    @FXML
+    void pdfBtn(MouseEvent event) {
+         // Afficher la boîte de dialogue de sélection de fichier
+         FileChooser fileChooser = new FileChooser();
+         fileChooser.setTitle("Enregistrer le fichier PDF");
+         fileChooser.getExtensionFilters().add(new ExtensionFilter("Fichiers PDF", "*.pdf"));
+         File selectedFile = fileChooser.showSaveDialog(((Node) event.getSource()).getScene().getWindow());
+         
+         if (selectedFile != null) {
+             // Générer le fichier PDF avec l'emplacement de sauvegarde sélectionné
+             // Récupérer la liste des produits
+            IProduitService produitService = new ProduitService();
+            List<Produit> productList = produitService.getAllProducts();
+            
+            try {
+                // Créer le document PDF
+                Document document = new Document();
+                PdfWriter.getInstance(document, new FileOutputStream(selectedFile));
+                document.open();
+
+                // Créer une instance de l'image
+                Image image = Image.getInstance("C:/Users/ALI/Desktop/ZeroWaste - JavaFx/zeroWaste-javaFX/src/assets/img/logo.png");
+
+                // Positionner l'image en haut à gauche
+                image.setAbsolutePosition(5, document.getPageSize().getHeight() - 120);
+
+                // Modifier la taille de l'image
+                image.scaleAbsolute(150, 150);
+
+                // Ajouter l'image au document
+                document.add(image);
+
+                // Créer une police personnalisée pour la date
+                Font fontDate = new Font(Font.FontFamily.TIMES_ROMAN, 18, Font.BOLD);
+                BaseColor color = new BaseColor(50, 187, 111); // Rouge: 50, Vert: 187, Bleu: 111
+                fontDate.setColor(color); // Définir la couleur de la police
+
+                // Créer un paragraphe avec le lieu
+                Paragraph tunis = new Paragraph("Tunis", fontDate);
+                tunis.setIndentationLeft(455); // Définir la position horizontale 
+                tunis.setSpacingBefore(-30); // Définir la position verticale 
+                // Ajouter le paragraphe au document
+                document.add(tunis);
+                
+                
+                // Obtenir la date d'aujourd'hui
+                LocalDate today = LocalDate.now();
+                
+                // Créer un paragraphe avec la date
+                Paragraph date = new Paragraph(today.toString(), fontDate);
+                
+                date.setIndentationLeft(437); // Définir la position horizontale 
+                date.setSpacingBefore(1); // Définir la position verticale
+                // Ajouter le paragraphe au document
+                document.add(date);
+                
+                // Créer une police personnalisée
+                Font font = new Font(Font.FontFamily.TIMES_ROMAN, 32, Font.BOLD);
+                BaseColor titleColor = new BaseColor(67, 136, 43); // 
+                font.setColor(titleColor);
+
+                // Ajouter le contenu au document
+                Paragraph title = new Paragraph("Liste des produits", font);
+                title.setAlignment(Element.ALIGN_CENTER);
+                title.setSpacingBefore(50); // Ajouter une marge avant le titre pour l'éloigner de l'image
+                title.setSpacingAfter(20);
+                document.add(title);
+                
+                PdfPTable table = new PdfPTable(6); // 6 colonnes pour les 6 attributs des produits
+                table.setWidthPercentage(100);
+                table.setSpacingBefore(30f);
+                table.setSpacingAfter(30f);
+                
+                // Ajouter les en-têtes de colonnes
+                Font hrFont = new Font(Font.FontFamily.TIMES_ROMAN, 16, Font.BOLD);
+                BaseColor hrColor = new BaseColor(50, 89, 74); // 
+                hrFont.setColor(hrColor);
+                
+                PdfPCell cell1 = new PdfPCell(new Paragraph("ID", hrFont));
+                BaseColor bgColor = new BaseColor(222, 254, 230);
+                cell1.setBackgroundColor(bgColor);
+                cell1.setBorderColor(titleColor);
+                cell1.setPaddingTop(20);
+                cell1.setPaddingBottom(20);
+                cell1.setHorizontalAlignment(Element.ALIGN_CENTER);
+
+                
+                PdfPCell cell2 = new PdfPCell(new Paragraph("Nom", hrFont));
+                cell2.setBackgroundColor(bgColor);
+                cell2.setBorderColor(titleColor);
+                cell2.setPaddingTop(20);
+                cell2.setPaddingBottom(20);
+                cell2.setHorizontalAlignment(Element.ALIGN_CENTER);
+
+                PdfPCell cell3 = new PdfPCell(new Paragraph("Quantité", hrFont));
+                cell3.setBackgroundColor(bgColor);
+                cell3.setBorderColor(titleColor);
+                cell3.setPaddingTop(20);
+                cell3.setPaddingBottom(20);
+                cell3.setHorizontalAlignment(Element.ALIGN_CENTER);
+
+                PdfPCell cell4 = new PdfPCell(new Paragraph("Prix", hrFont));
+                cell4.setBackgroundColor(bgColor);
+                cell4.setBorderColor(titleColor);
+                cell4.setPaddingTop(20);
+                cell4.setPaddingBottom(20);
+                cell4.setHorizontalAlignment(Element.ALIGN_CENTER);
+
+                PdfPCell cell5 = new PdfPCell(new Paragraph("Points", hrFont));
+                cell5.setBackgroundColor(bgColor);
+                cell5.setBorderColor(titleColor);
+                cell5.setPaddingTop(20);
+                cell5.setPaddingBottom(20);
+                cell5.setHorizontalAlignment(Element.ALIGN_CENTER);
+
+                PdfPCell cell6 = new PdfPCell(new Paragraph("Categorie", hrFont));
+                cell6.setBackgroundColor(bgColor);
+                cell6.setBorderColor(titleColor);
+                cell6.setPaddingTop(20);
+                cell6.setPaddingBottom(20);
+                cell6.setHorizontalAlignment(Element.ALIGN_CENTER);
+                
+                
+                table.addCell(cell1);
+                table.addCell(cell2);
+                table.addCell(cell3);
+                table.addCell(cell4);
+                table.addCell(cell5);
+                table.addCell(cell6);
+                
+                Font hdFont = new Font(Font.FontFamily.TIMES_ROMAN, 14, Font.NORMAL);
+                BaseColor hdColor = new BaseColor(50, 89, 74); // 
+                hrFont.setColor(hdColor);
+                // Ajouter les données des produits
+                for (Produit produit : productList) {
+                    PdfPCell cellR1 = new PdfPCell(new Paragraph(String.valueOf(produit.getId()), hdFont));
+                    cellR1.setBorderColor(titleColor);
+                    cellR1.setPaddingTop(10);
+                    cellR1.setPaddingBottom(10);
+                    cellR1.setHorizontalAlignment(Element.ALIGN_CENTER);
+                    table.addCell(cellR1);
+
+                    PdfPCell cellR2 = new PdfPCell(new Paragraph(produit.getNom_produit(), hdFont));
+                    cellR2.setBorderColor(titleColor);
+                    cellR2.setPaddingTop(10);
+                    cellR2.setPaddingBottom(10);
+                    cellR2.setHorizontalAlignment(Element.ALIGN_CENTER);
+                    table.addCell(cellR2);
+
+                    PdfPCell cellR3 = new PdfPCell(new Paragraph(String.valueOf(produit.getQuantite()), hdFont));
+                    cellR3.setBorderColor(titleColor);
+                    cellR3.setPaddingTop(10);
+                    cellR3.setPaddingBottom(10);
+                    cellR3.setHorizontalAlignment(Element.ALIGN_CENTER);
+                    table.addCell(cellR3);
+
+                    PdfPCell cellR4 = new PdfPCell(new Paragraph(String.valueOf(produit.getPrix_produit()), hdFont));
+                    cellR4.setBorderColor(titleColor);
+                    cellR4.setPaddingTop(10);
+                    cellR4.setPaddingBottom(10);
+                    cellR4.setHorizontalAlignment(Element.ALIGN_CENTER);
+                    table.addCell(cellR4);
+
+                    PdfPCell cellR5 = new PdfPCell(new Paragraph(String.valueOf(produit.getPrix_point_produit()), hdFont));
+                    cellR5.setBorderColor(titleColor);
+                    cellR5.setPaddingTop(10);
+                    cellR5.setPaddingBottom(10);
+                    cellR5.setHorizontalAlignment(Element.ALIGN_CENTER);
+                    table.addCell(cellR5);
+
+                    PdfPCell cellR6 = new PdfPCell(new Paragraph(produitService.getCategory(produit.getCategorie_produit_id()), hdFont));
+                    cellR6.setBorderColor(titleColor);
+                    cellR6.setPaddingTop(10);
+                    cellR6.setPaddingBottom(10);
+                    cellR6.setHorizontalAlignment(Element.ALIGN_CENTER);
+                    table.addCell(cellR6);
+                }
+                table.setSpacingBefore(20);
+                document.add(table);
+                document.close();
+                
+                System.out.println("Le fichier PDF a été généré avec succès.");
+            } catch (Exception e) {
+                e.printStackTrace();
+            }
+         }
+    
     }
     
 }
